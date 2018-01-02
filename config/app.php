36e9cdf302b162393934eb469e2ce3d8034fd585
Alex <a.feyertag@gmail.com>
2018-01-02 19:55:00 +0100

    Server config.

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://ftp68.world4you.com'),
 
     /*
     |--------------------------------------------------------------------------
