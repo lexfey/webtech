d8ac3a5b8e9a1c4a5723b84f3cedee1f2de3bea7
Alex <a.feyertag@gmail.com>
2018-02-22 17:46:27 +0100

    Mailversand

--- resources/views/email/sendOrderMail.blade.php
+++ resources/views/email/sendOrderMail.blade.php
@@ -10,21 +10,15 @@
 
 <h1>ORDER CONFIRMATION</h1>
 
-<h2>Hi, {{ $order->name }}</h2>
+<h2>Hi {{ $order->firstName }} {{ $order->lastName }},</h2>
 
 <p>Thank you for shopping with us!</p>
+<p>Your order number is {{$order->id}}</p>
+<p>In case of any questions, please don't reply to this email, but send us a message on {{ HTML::mailto('gruppe4webtech@gmail.com') }}</p>
+
 
 <h2>Order details</h2>
 <h3>Check your order details here: <a href="http://feyertag.family/orders">MY ORDERS</a></h3>
 <p></p>
-<p>Bitte überweisen Sie den Rechnungsbetrag auf untenstehendes Konto</p>
 
-<p>Bankverbindung: Mustermann GmbH
-Bankname: Deutsche Bank
-Kontonummer: 0012345678
-Bankleitzahl: 12345678
-IBAN * (int. Bankkontonummer) DE11123456780012345678
-BIC * (int. BLZ) DB12345
-Verwendungszweck: RG-2013-100524
-    Mustermann GmbH • E-Mail: webmaster@dw-formmailer.de • Web: https://www.dw-formmailer.de</p>
 
